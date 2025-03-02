09e06ab96c2ddd23ca8ddd65e1445f5ccf40e4d8
Kevin Hubert <kevin.hubert@buildasign.com>
2025-03-02 12:07:50 -0600

    CRA is sunsetting wtf?

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/api/hello")
+      .then((response) => response.json())
+      .then((data) => setMessage(data.message))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>React + Flask</h1>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default App;
